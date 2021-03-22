6f5659aa06ed84d1eb110449ca89b0a1a3f6f041
SRES0 <susanarepo@gmail.com>
2021-03-22 18:29:17 -0500

    form data/gif bank/gif template/updated constants

---
+++ lib/constants/gif_bank.dart
@@ -0,0 +1,10 @@
+// import 'package:atenciones_v2/models/gif.dart';
+
+// class GifBank {
+//   List<Gif> gifs = [
+//     Gif(
+//       name: 'apellidos',
+//       frames: 15,
+//     ),
+//   ];
+// }
