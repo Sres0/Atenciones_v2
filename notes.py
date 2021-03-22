12be5de46b0add5926051a47736cdb2916c4a2e1
SRES0 <susanarepo@gmail.com>
2021-03-22 16:52:38 -0500

    added notes

--- notes.py
+++ notes.py
@@ -1,4 +1,8 @@
 APP GENERAL
-    main.dart
-        Eliminar MyHomePage
-        
+    Validators/validation.dart
+        14: m[á]s
+
+ATENCIONES V1
+    Cambiar appointment_main por individual_appointment
+
+ATENCIONES V2
