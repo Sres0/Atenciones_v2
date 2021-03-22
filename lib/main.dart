c0bf1e976a709e1a4183c80bfe63aa4aaef557cd
SRES0 <susanarepo@gmail.com>
2021-03-22 15:06:52 -0500

    MyApp -> LSC App

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(LSCApp());
 }
 
-class MyApp extends StatelessWidget {
+class LSCApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
